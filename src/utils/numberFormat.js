8ea428eb53ac3171889ffb5031e1a95fdbcf6e32
Vu <vu@Tiffanys-MacBook-Pro.local>
2021-09-08 01:23:28 -0700

    Add more doc

--- src/utils/numberFormat.js
+++ src/utils/numberFormat.js
@@ -1,14 +1,31 @@
 export const TOKEN_MAX_DECIMALS = 6
 export const TOKEN_MIN_DECIMALS = 2
 
+/**
+ * Format token value with thousand separator ',' using US locale
+ * @param {number} number
+ * @param {number} maxDecimals
+ * @returns {string}
+ */
 export const formatTokenVal = (number, maxDecimals) => {
     return parseFloat(number).toLocaleString('en-US', { maximumFractionDigits: maxDecimals })
 }
 
+/**
+ * Format USD currency
+ * @param {number} number
+ * @returns {string}
+ */
 export const formatUSD = (number) => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(parseFloat(number))
 }
 
+/**
+ * Format string numbers for doing Math on them
+ * @param {string} numberStr
+ * @param {number} maxDecimals
+ * @returns {number}
+ */
 export const formatNumberInput = (numberStr, maxDecimals) => {
     return parseFloat(parseFloat(numberStr).toFixed(maxDecimals))
 }
